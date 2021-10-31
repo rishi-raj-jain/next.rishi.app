c90887fed8fc37fcca1c28f4e1564b21df3ec96f
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2021-11-01 02:51:55 +0530

    React - Preact

--- next.config.js
+++ next.config.js
@@ -4,10 +4,23 @@ const imageOptions = {
   domains: ['a.storyblok.com', 'rishi-raj-jain-html-og-image-default.layer0.link'],
 }
 
+const webPackOptions = (config, { dev, isServer }) => {
+  // Replace React with Preact only in client production build
+  if (!dev && !isServer) {
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    })
+  }
+  return config
+}
+
 const __preLayer0Export = {
   reactStrictMode: true,
   layer0SourceMaps: true,
   images: imageOptions,
+  webpack: webPackOptions,
 }
 
 if (process.env.NODE_ENV !== 'production') {
