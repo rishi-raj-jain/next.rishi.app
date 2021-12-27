ad723f0eff940b8e8962b361b4d9a06ce8f19825
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2021-12-27 17:27:34 +0530

    Include links to more posts

--- components/blog/more-posts.js
+++ components/blog/more-posts.js
@@ -1,29 +1,29 @@
 import Link from 'next/link'
-import Image from '@/components/Image'
-import { useTheme } from 'next-themes'
-import { imageLink } from '@/lib/data'
-import { useEffect, useState } from 'react'
 import Prefetch from '@layer0/react/Prefetch'
 
 const MorePosts = ({ morePosts }) => {
-  const { theme } = useTheme()
-  const [mounted, setMounted]= useState(true)
-  const filteredPosts= morePosts.filter((item) => item.hasOwnProperty('name'))
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+  const filteredPosts = morePosts.filter((item) => item.hasOwnProperty('name'))
+
   return (
     filteredPosts.length > 0 && (
       <div className="flex flex-col">
+        <div className="text-sm mt-10 mb-5">
+          <span> More Posts &rarr; </span>
+        </div>
         {filteredPosts.map((item) => (
           <Link href={`/blog/${item.slug}`} key={item.slug}>
-            <Prefetch url={process.browser ? `/_next/data/${__NEXT_DATA__.buildId}/blog/${item.slug}.json` : `/blog/${item.slug}`}>
-              <a className="mt-10 block w-full" href={`/blog/${item.slug}`}>
-                <Image
-                  alt={item.name}
-                  title={item.name}
-                  src={`${imageLink}/api?title=${item.name}&image=${item.content.image}${mounted ? (theme == 'light' ? '' : '&mode=o') : ''}`}
-                />
+            <Prefetch
+              url={
+                process.browser
+                  ? `/_next/data/${__NEXT_DATA__.buildId}/blog/${item.slug}.json`
+                  : `/blog/${item.slug}`
+              }
+            >
+              <a
+                href={`/blog/${item.slug}`}
+                className="hover:underline mb-5 block w-full font-bold text-lg"
+              >
+                {item.name}
               </a>
             </Prefetch>
           </Link>
