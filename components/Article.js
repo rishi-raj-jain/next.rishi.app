1eba19f61eefafde274d06732d88343eaff44ec8
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-06-06 02:08:33 +0530

    Update Article.js

--- components/Article.js
+++ components/Article.js
@@ -1,23 +1,20 @@
 import Head from 'next/head'
 import { useTheme } from 'next-themes'
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Article = ({ post }) => {
-  const { theme } = useTheme()
+  const { theme, systemTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
+  
   useEffect(() => {
     setMounted(true)
   }, [])
+  
   return (
-    <Fragment>
-      <Head>
-        <link rel="stylesheet" media={mounted ? 'all' : 'print'} href={`/css/${mounted ? (theme === 'system' ? 'light' : theme) : 'light'}.css`} />
-        <noscript>
-          <link media="all" rel="stylesheet" href="/css/light.css" />
-        </noscript>
-      </Head>
+    <>
       <article className="prose mt-10 max-w-none text-sm dark:prose-light" dangerouslySetInnerHTML={{ __html: post.content.long_text }} />
-    </Fragment>
+      <link rel="stylesheet" href={`/css/${mounted ? (theme === 'system' ? systemTheme : theme) : 'light'}.css`} />
+    </>
   )
 }
 
