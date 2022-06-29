b50cfae2481bab8f808e835ad73e1e9f549caee7
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-06-29 19:36:05 +0530

    Update cv.js

--- pages/cv.js
+++ pages/cv.js
@@ -1,43 +1,53 @@
+import SEO from '@/components/Seo'
+
 const CV = ({ data }) => {
+   const SEODetails = {
+    title: `CV - Rishi Raj Jain`,
+    canonical: `https://rishi.app/cv`,
+    deploymentUrl: `https://rishi.app`
+  }
   return (
-    <div className="flex w-full flex-col items-center text-[14px]">
-      <div className="mt-10 flex w-[90vw] max-w-[540px] flex-col">
-        <h2 className="text-3xl font-bold text-zinc-700 dark:text-gray-300">Rishi Raj Jain</h2>
-        <h2 id="About" className="mt-10 animate-fade text-zinc-700 dark:text-gray-300">
-          About
-        </h2>
-        <p className="mt-2 animate-fade-2 font-light text-slate-600 dark:text-slate-400">
-          Customer Success Manager at{' '}
-          <a target="_blank" className="text-black underline dark:text-slate-200" href="https://edg.io">
-            Edgio
-          </a>
-          .{' '}
-          <a target="_blank" className="text-black underline dark:text-slate-200" href="https://storyblok.com">
-            Storyblok
-          </a>{' '}
-          Ambassador. Synchronising my knowledge with community about Web Development, Caching, Edge Computing, Serverless, front-end ecosystems.
-        </p>
-      </div>
-      {Object.keys(data).map((i) => (
-        <div key={i} className="mt-10 flex w-[90vw] max-w-[540px] flex-col gap-y-5">
-          <h2 id={i} className="animate-fade text-zinc-700 dark:text-gray-300">
-            {i}
+    <Fragment>
+      <SEO {...SEODetails} />
+      <div className="flex w-full flex-col items-center text-[14px]">
+        <div className="mt-10 flex w-[90vw] max-w-[540px] flex-col">
+          <h2 className="text-3xl font-bold text-zinc-700 dark:text-gray-300">Rishi Raj Jain</h2>
+          <h2 id="About" className="mt-10 animate-fade text-zinc-700 dark:text-gray-300">
+            About
           </h2>
-          {data[i].map((j, _ind) => (
-            <div key={_ind} className="flex flex-col gap-y-2 gap-x-10 md:flex-row md:gap-y-0">
-              <p className="min-w-[100px] animate-fade-2 font-light text-gray-400">{j.name}</p>
-              <a
-                target="_blank"
-                href={j.href || '#'}
-                className="flex animate-fade-3 flex-row items-center justify-start font-light text-black hover:underline dark:text-slate-200"
-              >
-                {j.title} &#x2197;
-              </a>
-            </div>
-          ))}
+          <p className="mt-2 animate-fade-2 font-light text-slate-600 dark:text-slate-400">
+            Customer Success Manager at{' '}
+            <a target="_blank" className="text-black underline dark:text-slate-200" href="https://edg.io">
+              Edgio
+            </a>
+            .{' '}
+            <a target="_blank" className="text-black underline dark:text-slate-200" href="https://storyblok.com">
+              Storyblok
+            </a>{' '}
+            Ambassador. Synchronising my knowledge with community about Web Development, Caching, Edge Computing, Serverless, front-end ecosystems.
+          </p>
         </div>
-      ))}
-    </div>
+        {Object.keys(data).map((i) => (
+          <div key={i} className="mt-10 flex w-[90vw] max-w-[540px] flex-col gap-y-5">
+            <h2 id={i} className="animate-fade text-zinc-700 dark:text-gray-300">
+              {i}
+            </h2>
+            {data[i].map((j, _ind) => (
+              <div key={_ind} className="flex flex-col gap-y-2 gap-x-10 md:flex-row md:gap-y-0">
+                <p className="min-w-[100px] animate-fade-2 font-light text-gray-400">{j.name}</p>
+                <a
+                  target="_blank"
+                  href={j.href || '#'}
+                  className="flex animate-fade-3 flex-row items-center justify-start font-light text-black hover:underline dark:text-slate-200"
+                >
+                  {j.title} &#x2197;
+                </a>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Fragment>
   )
 }
 
