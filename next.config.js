84ae15505994a61e7d5edc69e7f475468ae8f690
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-08-12 01:37:52 +0530

    Add rishi.app to valid domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   output: 'standalone',
-  images: { domains: ['localhost', 'a.storyblok.com', 'rishi-raj-jain-html-og-image-default.layer0-limelight.link'] },
+  images: { domains: ['localhost', 'a.storyblok.com', 'rishi.app', 'rishi-raj-jain-html-og-image-default.layer0-limelight.link'] },
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
