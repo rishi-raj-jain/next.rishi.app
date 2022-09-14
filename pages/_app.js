5a90c63c861c1ad4685bffe24841942824af5899
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-09-14 22:23:29 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
-import '@/styles/global.css'
-import { useEffect } from 'react'
 import Layer0RUM from 'layer0/rum'
 import Navbar from '@/components/Navbar'
 import { ThemeProvider } from 'next-themes'
+import { install } from '@layer0/prefetch/window'
 
 if (process.env.NODE_ENV == 'production') {
   Layer0RUM('16dbca52-84e4-4087-96d7-1d0aab0c4421')
 }
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    install()
+  }, [])
   return (
     <ThemeProvider attribute="class">
       <div className="flex flex-col items-center bg-white font-display text-black transition-colors duration-200 dark:bg-black dark:text-gray-200">
