589e267ad7ab609b42bd593221565eeff6b90c2d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-12-16 18:59:09 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,6 @@ const Document = ({}) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="google-site-verification" content="hgb7sEPSlo0We6rT4UtpV04_qCFTryuwkcSEdtmAxr8" />
-        <script
-          async
-          crossOrigin="anonymous"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3793400768853434"
-        ></script>
       </Head>
       <body>
         <Main />
