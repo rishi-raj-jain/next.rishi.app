6159240f60656623566b13fe238de9e2bb92c0ee
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-09-11 01:40:37 +0530

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   semi: false,
-  printWidth: 150,
+  // printWidth: 150,
   singleQuote: true,
   tailwindConfig: './tailwind.config.js',
   plugins: [require('prettier-plugin-tailwindcss')],
