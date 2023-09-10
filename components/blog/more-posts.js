d8211cc7ec2878f940c0419fbe4ef74c9a582074
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-09-11 02:05:06 +0530

    Update more-posts.js

--- components/blog/more-posts.js
+++ components/blog/more-posts.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link
 import { useEffect, useState } from 'react'
 import { prefetch } from '@edgio/prefetch/window'
 import { createNextDataURL } from '@/lib/operations'
